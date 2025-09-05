94d46786bf4054de23f5730be046aba21f81a21a
dzammit22 <zammitdan@gmail.com>
2025-09-05 16:59:57 +0200

    Delete src directory

--- src/storage.js
+++
@@ -1,9 +0,0 @@
-const SAVE_KEY = 'vital-tasks-save-mod-1';
-
-export function save(state){
-  localStorage.setItem(SAVE_KEY, JSON.stringify(state));
-}
-export function load(){
-  try{ return JSON.parse(localStorage.getItem(SAVE_KEY)||''); }
-  catch(e){ return null; }
-}
